6b8fe9b6e30384d4590ed8bfb459677062441fc4
Kamil Marcinow <kamil.marcinow@gmail.com>
2019-02-17 09:14:30 +0100

    first

---
+++ src/main/java/XXX/App.java
@@ -0,0 +1,17 @@
+package XXX;
+
+import java.util.logging.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    protected static final Logger log = Logger.getLogger(Logger.class.getName());
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
